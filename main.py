2368f6b57afaadc093b31c1bfd64d2a05e6c6ebf
karolis.nakutavicius1@gmail.com <karolis.nakutavicius1@gmail.com>
2022-11-04 14:15:28 +0200

    fix

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ def convert_to_applicable_types():
     df[PRICE_COLUMN_NAME] = df[PRICE_COLUMN_NAME].str.replace(',', '.')
     df[PRICE_COLUMN_NAME] = pd.to_numeric(df[PRICE_COLUMN_NAME])
     df[DATE_COLUMN_NAME] = pd.to_datetime(df[DATE_COLUMN_NAME], dayfirst=True)
-    df[DATE_COLUMN_NAME] = df[DATE_COLUMN_NAME].apply(lambda x: x.Year)
+
+
+def show_graph():
+    plt.plot(df[DATE_COLUMN_NAME], df[PRICE_COLUMN_NAME])
+    plt.title("Electricity price")
+    plt.ylabel('MWh')
+    plt.xlabel('Day')
+    plt.tick_params(axis='x', which='major', labelsize=5)
+    plt.show()
 
 
 df = pd.read_csv(r'C:\Users\karol\Desktop\Dienos-kainos.csv')
 convert_to_applicable_types()
+# show_graph()
 
-plt.plot(df[DATE_COLUMN_NAME], df[PRICE_COLUMN_NAME])
-plt.title("Electricity price")
-plt.ylabel('MWh')
-plt.xlabel('Day')
-plt.tick_params(axis='x', which='major', labelsize=5)
-
-plt.show()
+df.dropna(inplace=True)
+df.columns = ['ds', 'y']
+print(df.head())
