15b7f97f06475f14f14498867b955cdc9d812e3e
karolis.nakutavicius1@gmail.com <karolis.nakutavicius1@gmail.com>
2022-11-04 14:10:14 +0200

    usings consants

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from neuralprophet import NeuralProphet
 import pandas as pd
 from matplotlib import pyplot as plt
 
+PRICE_COLUMN_NAME = 'Value'
+DATE_COLUMN_NAME = 'Date'
+
 
 def convert_to_applicable_types():
-    df['Value'] = df['Value'].str.replace(',', '.')
-    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
-    df['Value'] = pd.to_numeric(df['Value'])
+    df[PRICE_COLUMN_NAME] = df[PRICE_COLUMN_NAME].str.replace(',', '.')
+    df[PRICE_COLUMN_NAME] = pd.to_numeric(df[PRICE_COLUMN_NAME])
+    df[DATE_COLUMN_NAME] = pd.to_datetime(df[DATE_COLUMN_NAME], dayfirst=True)
+    df[DATE_COLUMN_NAME] = df[DATE_COLUMN_NAME].apply(lambda x: x.Year)
 
 
 df = pd.read_csv(r'C:\Users\karol\Desktop\Dienos-kainos.csv')
 convert_to_applicable_types()
 
-print(df.dtypes)
+plt.plot(df[DATE_COLUMN_NAME], df[PRICE_COLUMN_NAME])
+plt.title("Electricity price")
+plt.ylabel('MWh')
+plt.xlabel('Day')
+plt.tick_params(axis='x', which='major', labelsize=5)
+
+plt.show()
