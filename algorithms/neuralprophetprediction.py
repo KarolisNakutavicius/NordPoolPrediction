03345cd4dd2523c03bc74e63db73e2bd72e432d8
karolis.nakutavicius1@gmail.com <karolis.nakutavicius1@gmail.com>
2022-11-15 11:03:11 +0200

    code reuse

--- algorithms/neuralprophetprediction.py
+++ algorithms/neuralprophetprediction.py
@@ -1,20 +1,8 @@
 from neuralprophet import NeuralProphet
-import pandas as pd
-from matplotlib import pyplot as plt
-import os
-
-PRICE_COLUMN_NAME = 'Value'
-DATE_COLUMN_NAME = 'DateTime'
-
-
-def convert_to_applicable_types(df):
-    df[PRICE_COLUMN_NAME] = pd.to_numeric(df[PRICE_COLUMN_NAME])
-    df[DATE_COLUMN_NAME] = pd.to_datetime(df[DATE_COLUMN_NAME])
+import utilities
 
 
 def reformat_for_prophet():
-    convert_to_applicable_types(train_df)
-    convert_to_applicable_types(test_df)
     train_df.columns = ['ds', 'y']
     train_df.ds = train_df.ds.drop_duplicates()
     train_df.dropna(inplace=True)
@@ -22,21 +10,22 @@ def reformat_for_prophet():
     test_df.ds = test_df.ds.drop_duplicates()
     test_df.dropna(inplace=True)
 
-
-train_df = pd.read_csv(rf'{os.getcwd()}\trainingData.csv')
-test_df = pd.read_csv(rf'{os.getcwd()}\testingData2022.csv')
-
+# Init
+train_df, test_df = utilities.get_data()
+utilities.setup_plot_with_data(train_df, test_df)
 reformat_for_prophet()
 
+# Train
 m = NeuralProphet()
 train_metrics = m.fit(train_df, freq='D')
+
+# Test
 test_metrics = m.test(test_df)
 print(f'============ TEST RESULTS ============\n\n{test_metrics}\n\n====================================\n\n')
 
+# Predict
 forecast = m.predict(test_df)
 print(forecast)
 
 plot = m.plot(forecast)
-plt.ylabel('MWh')
-plt.xlabel('Day')
 plot.show()
