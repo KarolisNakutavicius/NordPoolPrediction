1b9ad27bbc7a61fdeef41257ffaf82f539194948
karolis.nakutavicius1@gmail.com <karolis.nakutavicius1@gmail.com>
2022-11-21 08:48:51 +0200

    plot method cleanup

--- algorithms/lstm_prediction.py
+++ algorithms/lstm_prediction.py
@@ -34,23 +34,35 @@ def convert_to_samples_and_labels(df, window_size=WINDOW_SIZE):
     return np.array(samples), np.array(labels)
 
 
-def train_model():
+def plot_predictions(used_model, sample, label, start=0, end=100):
+    train_predictions = used_model.predict(sample).flatten()
+    used_model.evaluate(sample, label, verbose=2)
 
-    #Figure 9 shows that the optimal results can be achieved using three layers
+    results = pd.DataFrame(data={'Train Predictions': train_predictions, 'Actuals': label})
+
+    print(results)
+    #
+    plt.plot(results['Train Predictions'][start:end])
+    plt.plot(results['Actuals'][start:end])
+    plt.show()
+
+
+def train_model():
+    # Figure 9 shows that the optimal results can be achieved using three layers
     model = Sequential()
     model.add(InputLayer((WINDOW_SIZE, 1)))  # Or keras.layers.Flatten()
     model.add(LSTM(64))  # 64?? Define meaning
     model.add(Dense(8, 'relu'))
-    model.add(Dense(1, 'linear'))  #Define
+    model.add(Dense(1, 'linear'))  # Define
     cp = ModelCheckpoint(MODEL_PATH, save_best_only=True)
     model.compile(
         loss=MeanAbsoluteError(),
-        optimizer=Adam(learning_rate=0.001, clipnorm=1),
+        optimizer=Adam(learning_rate=0.001),
         metrics=[
             MeanAbsoluteError(),
             MeanAbsolutePercentageError(),
             RootMeanSquaredError()
-                  ])
+        ])
     print(model.summary())
     model.fit(samples_train, label_train, validation_data=(samples_test, label_test), epochs=20,
               callbacks=[cp])  # Diffs between validtion and train data
@@ -62,9 +74,8 @@ train_df, test_df = utilities.init_data()
 samples_train, label_train = convert_to_samples_and_labels(train_df[constants.PRICE_COLUMN_NAME])
 samples_test, label_test = convert_to_samples_and_labels(test_df[constants.PRICE_COLUMN_NAME])
 
-#TRAIN NEW MODEL
+# TRAIN NEW MODEL
 model = train_model()
-
 
 
 # Load Model
@@ -72,18 +83,10 @@ model = train_model()
 #                    custom_objects={
 #                        'MeanAbsoluteError': MeanAbsoluteError(),
 #                        'MeanAbsolutePercentageError': MeanAbsolutePercentageError(),
-#                        'RootMeanSquaredError': RootMeanSquaredError()})
+#                        'RootMeanSquaredError': RootMeanSquaredError()})4
 
-train_predictions = model.predict(samples_test).flatten()
-model.evaluate(samples_test, label_test, verbose=2)
 
-results = pd.DataFrame(data={'Train Predictions': train_predictions, 'Actuals': label_test})
-
-print(results)
-#
-plt.plot(results['Train Predictions'][:200])
-plt.plot(results['Actuals'][:200])
-plt.show()
+plot_predictions(model, samples_test, label_test)
 
 # train_predictions = model1.predict(X_val).flatten()
 # train_results = pd.DataFrame(data={'Train Predictions': train_predictions, 'Actuals': y_val})
